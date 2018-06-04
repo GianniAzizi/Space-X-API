e874d3a0e97336022acd6aea2d2e0276f650c0b9
Gianni Azizi <gianni.azizi@bnpparibas-pf.com>
2018-06-04 14:20:24 +0200

    Fix heroku configuration

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/Space-X'));
 
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname+'/dist/Space-X/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);

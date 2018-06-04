7800128ca8034c7ad863d7af1dcf5dc7b0cb10cd
Gianni Azizi <gianni.azizi@bnpparibas-pf.com>
2018-06-04 16:22:57 +0200

    Navbar + router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NavbarComponent} from './navbar/navbar.component';
+import {MatIconModule, MatNavList, MatSidenavModule, MatToolbar} from '@angular/material';
+import {RouterModule, Routes} from '@angular/router';
+import { MissionComponent } from './mission/mission.component';
+import { LaunchesComponent } from './launches/launches.component';
+
+const appRoutes: Routes = [
+  {path: 'missions', component: MissionComponent},
+  {path: 'launches', component: LaunchesComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    MatToolbar,
+    MatNavList,
+    MissionComponent,
+    LaunchesComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatIconModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]

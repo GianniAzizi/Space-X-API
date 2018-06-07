b907d8c6e76a15deea90965c7f46172ada5abef2
Gianni Azizi <gianni.azizi@bnpparibas-pf.com>
2018-06-07 16:44:24 +0200

    Add a dialog box to display google map

--- src/app/launchpads/launchpad/launchpad.module.ts
+++ src/app/launchpads/launchpad/launchpad.module.ts
@@ -4,20 +4,25 @@ import {AgmCoreModule} from '@agm/core';
 import {GooglemapsComponent} from '../../googlemaps/googlemaps.component';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouterModule} from '@angular/router';
+import {GoogleMapDialogComponent} from '../googlemap-dialog/googlemap-dialog.component';
+import {MaterialModule} from '../../material/material.module';
 @NgModule({
   declarations: [
     LaunchpadComponent,
-    GooglemapsComponent
+    GooglemapsComponent,
+    GoogleMapDialogComponent
   ],
   imports: [
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyC2JTwuEuSMGvZgOdkBQaLkQ7j78l_EGII'
     }),
     BrowserModule,
-    RouterModule
+    RouterModule,
+    MaterialModule
   ],
   exports: [
     LaunchpadComponent
-  ]
+  ],
+  entryComponents: [GoogleMapDialogComponent]
 })
 export class LaunchpadModule {}
